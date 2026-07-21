c5afa7ae6ff7958e98afe8329080a7792a5da78b
techiebella <reddieswari3@gmail.com>
2026-07-21 21:20:34 +0530

    Update

--- library_project/settings.py
+++ library_project/settings.py
@@ -8,14 +8,18 @@ from pathlib import Path
 import os
 import dj_database_url
 
+
 # --------------------------------------------------
 # Base Directory
 # --------------------------------------------------
+
 BASE_DIR = Path(__file__).resolve().parent.parent
+
 
 # --------------------------------------------------
 # Security
 # --------------------------------------------------
+
 SECRET_KEY = os.environ.get(
     "SECRET_KEY",
     "django-insecure-bpe8^#p1apo*-)3mh!og96y*@b&#$zaxnv=9b0u#$e6&(v=-#o"
@@ -25,111 +29,197 @@ DEBUG = os.environ.get("DEBUG", "False") == "True"
 
 ALLOWED_HOSTS = ["*"]
 
+
 # --------------------------------------------------
 # Installed Apps
 # --------------------------------------------------
-INSTALLED_APPS = [
-    'library_app',
 
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
+INSTALLED_APPS = [
+
+    # Local apps
+    "library_app",
+
+    # Django apps
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
 ]
+
 
 # --------------------------------------------------
 # Middleware
 # --------------------------------------------------
-MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
 
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+MIDDLEWARE = [
+
+    "django.middleware.security.SecurityMiddleware",
+
+    # WhiteNoise for static files in Render
+    "whitenoise.middleware.WhiteNoiseMiddleware",
+
+    "django.contrib.sessions.middleware.SessionMiddleware",
+
+    "django.middleware.common.CommonMiddleware",
+
+    "django.middleware.csrf.CsrfViewMiddleware",
+
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+
+    "django.contrib.messages.middleware.MessageMiddleware",
+
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = 'library_project.urls'
+
+# --------------------------------------------------
+# URL Configuration
+# --------------------------------------------------
+
+ROOT_URLCONF = "library_project.urls"
+
 
 # --------------------------------------------------
 # Templates
 # --------------------------------------------------
+
 TEMPLATES = [
+
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / "library_app" / "templates"],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+
+        "DIRS": [
+            BASE_DIR / "library_app" / "templates"
+        ],
+
+        "APP_DIRS": True,
+
+        "OPTIONS": {
+
+            "context_processors": [
+
+                "django.template.context_processors.request",
+
+                "django.contrib.auth.context_processors.auth",
+
+                "django.contrib.messages.context_processors.messages",
+
             ],
         },
     },
 ]
 
-WSGI_APPLICATION = 'library_project.wsgi.application'
+
+WSGI_APPLICATION = "library_project.wsgi.application"
+
 
 # --------------------------------------------------
 # Database
 # --------------------------------------------------
+
 DATABASES = {
+
     "default": dj_database_url.config(
+
         default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+
         conn_max_age=600,
+
     )
 }
+
 
 # --------------------------------------------------
 # Password Validation
 # --------------------------------------------------
+
 AUTH_PASSWORD_VALIDATORS = [
+
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        "NAME":
+        "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
+
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        "NAME":
+        "django.contrib.auth.password_validation.MinimumLengthValidator",
     },
+
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        "NAME":
+        "django.contrib.auth.password_validation.CommonPasswordValidator",
     },
+
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        "NAME":
+        "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
+
 ]
+
 
 # --------------------------------------------------
 # Internationalization
 # --------------------------------------------------
-LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+LANGUAGE_CODE = "en-us"
+
+TIME_ZONE = "Asia/Kolkata"
 
 USE_I18N = True
 
 USE_TZ = True
 
+
+
 # --------------------------------------------------
 # Static Files
 # --------------------------------------------------
+
 STATIC_URL = "/static/"
+
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
+# If you have project static folder
+STATICFILES_DIRS = [
+
+    BASE_DIR / "static",
+
+]
+
+
+STATICFILES_STORAGE = (
+    "whitenoise.storage.CompressedManifestStaticFilesStorage"
+)
+
+
 
 # --------------------------------------------------
-# Media Files
+# Media Files (Images Upload)
 # --------------------------------------------------
+
 MEDIA_URL = "/media/"
+
 MEDIA_ROOT = BASE_DIR / "media"
+
+
+
+# --------------------------------------------------
+# Authentication Redirects
+# --------------------------------------------------
+
+LOGIN_URL = "/student/login/"
+
+LOGIN_REDIRECT_URL = "/student/dashboard/"
+
+LOGOUT_REDIRECT_URL = "/"
+
 
 # --------------------------------------------------
 # Default Primary Key
 # --------------------------------------------------
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
